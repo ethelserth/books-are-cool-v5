096c45536126048320081df0cbcf7389a1b9819b
ethelserth <pergaminh@live.com>
2023-03-04 21:09:09 +0200

    notion id new

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b16a116291e74362b08586772698ac56',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
